4060ba5e914ee7f98aac454c3daa1895140d45f2
Marshal <marshalaldham97@gmail.com>
2026-04-10 22:44:44 +0100

    fix google oauth callback url to use APP_BASE_URL

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ const twilioClient = twilio(
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
+  process.env.APP_BASE_URL + "/google/callback"
 );
 
 /* ---------------- HELPERS ---------------- */
